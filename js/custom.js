b2a109bd8b0c06e15e0611aa2efd1dec2aa684ac
Paco Alías <dtodo1paco@gmail.com>
2020-05-08 10:47:36 +0200

    Update custom.js
    change online date

--- js/custom.js
+++ js/custom.js
@@ -7,7 +7,7 @@ Theme by: WebThemez.com
 Note: Please use our back link in your site
 */
 $( function() {
-        var endDate = "May  15, 2020 15:03:25";
+        var endDate = "Jun  15, 2020 15:00:00";
 
         $('.countdown.simple').countdown({ date: endDate });
 
