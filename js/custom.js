dd4c75c776b31f68af2887666990231d7cc65208
Paco Alías <dtodo1paco@gmail.com>
2020-10-21 16:52:00 +0200

    looking for spring of 2021

--- js/custom.js
+++ js/custom.js
@@ -7,7 +7,7 @@ Theme by: WebThemez.com
 Note: Please use our back link in your site
 */
 $( function() {
-        var endDate = "Sep 21, 2020 15:00:00";
+        var endDate = "Mar 2, 2021 15:00:00";
 
         $('.countdown.simple').countdown({ date: endDate });
 
