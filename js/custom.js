a60bef77445249c0b78d6f2d0ad3a7d4006fd2b6
Paco Alías <dtodo1paco@gmail.com>
2021-07-20 08:43:41 +0200

    3 months more

--- js/custom.js
+++ js/custom.js
@@ -7,7 +7,7 @@ Theme by: WebThemez.com
 Note: Please use our back link in your site
 */
 $( function() {
-        var endDate = "Mar 2, 2021 15:00:00";
+        var endDate = "Oct 2, 2021 15:00:00";
 
         $('.countdown.simple').countdown({ date: endDate });
 
